ae3f6c67512faf8d09b3758ac870dc6cd6f35e2d
Dawin Ye <yedawin@gmail.com>
2020-04-12 22:41:59 -0700

    9.2

---
+++ Snacktacular/SpotsListViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by John Gallaugher on 3/23/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+// IMPORTANT!!!
+// Below is a replacement for the func signIn() shown in the video for Ch. 9.3 in Textbook v.3. AFTER entering func signIn() as shown in the video, uncomment the function below, cut it from this location, and paste it over the func signIn() that you just wrote. You can delete this comment once you've replaced func signIn()
+//
+//func signIn() {
+//    let providers: [FUIAuthProvider] = [
+//        FUIGoogleAuth(),
+//    ]
+//    let currentUser = authUI.auth?.currentUser
+//    if authUI.auth?.currentUser == nil {
+//        self.authUI?.providers = providers
+//        let loginViewController = authUI.authViewController()
+//        loginViewController.modalPresentationStyle = .fullScreen
+//        present(loginViewController, animated: true, completion: nil)
+//    } else {
+//        tableView.isHidden = false
+//    }
+//}
+
+import UIKit
+import CoreLocation
+
+class SpotsListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var spots: Spots!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        spots = Spots()
+        spots.spotArray.append(Spot(name: "El Pelon", address: "Comm Ave", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+        spots.spotArray.append(Spot(name: "Yamato's", address: "Comm Ave", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+        spots.spotArray.append(Spot(name: "BBQ Chicken", address: "Harv Ave", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowSpot" {
+            let destination = segue.destination as! SpotDetailViewController
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.spot = spots.spotArray[selectedIndexPath.row]
+            
+        } else {
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                tableView.deselectRow(at: selectedIndexPath, animated: true)
+            }
+        }
+    }
+}
+extension SpotsListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spots.spotArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SpotsTableViewCell
+        cell.nameLabel.text = spots.spotArray[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
